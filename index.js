dc43e0e6ab76342430a0bf25b0eae647a4cd6936
Stephen Adler <adlercreations@gmail.com>
2024-07-21 15:31:41 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,8 +1,87 @@
 // Write your code here!
-document.querySelector("main").remove()
+const mainNode = document.getElementById(`main`);
+mainNode.remove()
 
-const newHeader= document.createElement("h1")
+const newHeader = document.createElement(`h1`)
 
-newHeader.setAttribute("id", "victory")
+//newHeader.setAttribute(`id`, `victory`)
+newHeader.id = "victory"
 
 newHeader.textContent = "Stephen is the champion"
+
+//newHeader.innerHTML = "Stephen is the champion"
+
+// function makeChampion(champName) {
+//     return newHeader.textContent = (`${champName} is the champion`)
+// }
+// makeChampion = ("Stephen")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// documentquerySelector("main").remove()
+
+// constnewHeader= document.createElement("h1")
+
+// newHeadersetAttribute("id", "victory")
+
+// newHeadertextContent = "Stephen is the champion"
